e45e0533771d49ab5e2a27c5478480bd8944ddb3
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-21 20:29:02 +0100

    do some exercises to test

--- test/Factory.js
+++ test/Factory.js
@@ -1,7 +1,18 @@
-const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers")
-const { expect } = require("chai")
-const { ethers } = require("hardhat")
+const {
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Factory", function () {
-
-})
+  it("should have a name -test", async function () {
+    //fetch the contract
+    const Factory = await ethers.getContractFactory("Factory");
+    //deploy the contract
+    const factory = await Factory.deploy();
+    //check name
+    const name = await factory.name();
+    //check name is correct
+    expect(name).to.equal("Factory");
+  });
+});
