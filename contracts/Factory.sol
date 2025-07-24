ef9ea91c307cd42fa6f3b93603e64d07dc7cf8ea
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-24 19:13:04 +0100

    create a token and make tests about the deployment and creation

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,6 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.27;
 
+import {Token} from "./Token.sol";
+
 contract Factory {
-    string public name = "Factory";
+    uint256 public immutable fee;
+    address public owner;
+
+    uint256 public totalTokens;
+    address[] public tokens;
+
+    constructor(uint256 _fee) {
+        fee = _fee;
+        owner = msg.sender;
+    }
+
+    function create(
+        string memory _name,
+        string memory _symbol
+    ) external payable {
+        //create a new token
+        Token token = new Token(msg.sender, _name, _symbol, 1_000_000 ether);
+        //save the token
+        tokens.push(address(token));
+
+        totalTokens++;
+        //list the tooken for sale
+        //tell people to buy
+    }
 }
--- test/Factory.js
+++ test/Factory.js
@@ -5,14 +5,49 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Factory", function () {
-  it("should have a name -test", async function () {
+  const FEE = ethers.parseUnits("0.01", 18);
+
+  async function deployFactoryFixture() {
+    //fetch the accounts
+    const [deployer, creator] = await ethers.getSigners();
     //fetch the contract
     const Factory = await ethers.getContractFactory("Factory");
     //deploy the contract
-    const factory = await Factory.deploy();
-    //check name
-    const name = await factory.name();
-    //check name is correct
-    expect(name).to.equal("Factory");
+
+    const factory = await Factory.deploy(FEE);
+
+    //create token
+    const transaction = await factory
+      .connect(creator)
+      .create("Dapp Uni", "DAPP", { value: FEE });
+
+    await transaction.wait();
+
+    //get token address
+    const tokenAddress = await factory.tokens(0);
+    const token = await ethers.getContractAt("Token", tokenAddress);
+
+    return { factory, deployer, creator, token };
+  }
+
+  describe("Deployment", function () {
+    it("Should set the fee", async function () {
+      const { factory } = await loadFixture(deployFactoryFixture);
+
+      expect(await factory.fee()).to.equal(FEE);
+    });
+
+    it("Should set the owner", async function () {
+      const { factory, deployer } = await loadFixture(deployFactoryFixture);
+
+      expect(await factory.owner()).to.equal(deployer.address);
+    });
+  });
+
+  describe("Creating", function () {
+    it("Should set the owner", async function () {
+      const { factory, token } = await loadFixture(deployFactoryFixture);
+      expect(await token.owner()).to.equal(await factory.getAddress());
+    });
   });
 });
