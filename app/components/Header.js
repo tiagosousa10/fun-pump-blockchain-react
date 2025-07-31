277688b9bcddfc1477a4c6fb9117b8e5714a77a9
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-31 20:18:24 +0100

    connect ethereum network with a wallet

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,10 +1,27 @@
 import { ethers } from "ethers";
 
 function Header({ account, setAccount }) {
+  async function connectHandler() {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = ethers.getAddress(accounts[0]);
+
+    setAccount(account);
+  }
+
   return (
     <header>
       <p className="brand">fun.pump</p>
-      <button className="btn--fancy">Hello world</button>
+      {account ? (
+        <button className="btn--fancy">
+          [{account.slice(0, 6) + "..." + account.slice(38, 42)}]
+        </button>
+      ) : (
+        <button onClick={connectHandler} className="btn--fancy">
+          [ connect ]
+        </button>
+      )}
     </header>
   );
 }
--- app/page.js
+++ app/page.js
@@ -15,12 +15,21 @@ import config from "./config.json";
 import images from "./images.json";
 
 export default function Home() {
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+
+  async function loadBlockchainData() {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    setProvider(provider);
+  }
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
+
   return (
     <div className="page">
-      <Header
-        account={"0x70997970C51812dc3A010C7d01b50e0d17dc79C8"}
-        setAccount={""}
-      />
+      <Header account={account} setAccount={setAccount} />
     </div>
   );
 }
